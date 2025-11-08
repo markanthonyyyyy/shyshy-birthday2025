32d164228c9f1ac981c08e1fbfa963132d1d8dd2
Mark Anthony Aguilar <markanthonyaguilar@digits.ph>
2025-11-09 03:29:36 +0800

    update photo

--- src/assets/Pages/Card1.jsx
+++ src/assets/Pages/Card1.jsx
@@ -12,10 +12,10 @@ const Card1 = ({ onReturn }) => {
         }}
       >
         <div className="grid md:grid-cols-3 gap-4 mb-8">
-          {[1, 2, 3, 4, 5, 6].map((i) => (
+          {[4, 2, 3, 1, 5, 6, 7, 8, 9, 11, 12].map((i) => (
             <div
               key={i}
-              className="border-4 border-blue-900 rounded-2xl p-4 bg-gradient-to-b from-sky-200 to-green-200 aspect-square transform hover:rotate-2 transition-all animate-fade-in"
+              className="border-4 border-pink-600 rounded-2xl p-4 bg-gradient-to-b from-pink-200 to-pink-400 aspect-square transform hover:rotate-2 transition-all animate-fade-in"
               style={{
                 animationDelay: `${0.3 + i * 0.1}s`,
                 opacity: 0,
@@ -23,7 +23,7 @@ const Card1 = ({ onReturn }) => {
               }}
             >
               <img
-                src={`/us/photo${i}.jpg`}
+                src={`/us/photo${i}.jpeg`}
                 alt={`photo${i}`}
                 className="w-full h-full object-cover rounded-lg"
               />
@@ -33,27 +33,15 @@ const Card1 = ({ onReturn }) => {
       </div>
 
       <div
-        className="border-t-2 border-dashed border-blue-900 w-full max-w-4xl animate-fade-in"
+        className="border-t-2 border-dashed border-pink-600 w-full max-w-4xl animate-fade-in"
         style={{
           animationDelay: "1s",
           opacity: 0,
           animationFillMode: "forwards",
         }}
       ></div>
-
-      <p
-        className="text-sm text-center animate-fade-in"
-        style={{
-          fontFamily: "Comic Relief, Aloja",
-          animationDelay: "1.2s",
-          opacity: 0,
-          animationFillMode: "forwards",
-        }}
-      >
-        your terlucu when we take pictures together :3
-      </p>
       <h1
-        className="text-3xl md:text-4xl font-bold text-center text-blue-900 animate-fade-in"
+        className="text-3xl md:text-4xl font-bold text-center text-pink-500 animate-fade-in"
         style={{
           fontFamily: "Aloja",
           animationDelay: "1.3s",
@@ -66,7 +54,7 @@ const Card1 = ({ onReturn }) => {
 
       <button
         onClick={onReturn}
-        className="border-2 font-bold uppercase border-blue-900 bg-pink-100 hover:bg-pink-200 rounded-full px-8 py-3 text-sm transition-all hover:scale-105 animate-fade-in"
+        className="border-2 font-bold text-pink-600 uppercase border-pink-600 bg-pink-100 hover:bg-pink-200 rounded-full px-8 py-3 text-sm transition-all hover:scale-105 animate-fade-in"
         style={{
           fontFamily: "Comic Relief, Aloja",
           animationDelay: "1.4s",
